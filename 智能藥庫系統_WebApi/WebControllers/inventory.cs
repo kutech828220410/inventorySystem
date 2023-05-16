4939487a0567ab8d2fb25fa67dbdeb1c489ff18d
Administrator <Administrator@WIN-K7GD1U2J94O>
2023-05-17 05:30:29 +0800

    20230517

--- 智能藥庫系統_WebApi/WebControllers/inventory.cs
+++ 智能藥庫系統_WebApi/WebControllers/inventory.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using MySql.Data.MySqlClient;
+using SQLUI;
+using Basic;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Configuration;
+using MyOffice;
+using NPOI;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using MyUI;
 namespace 智慧藥庫系統_WebApi
 {
-    public class inventory
+    [Route("api/[controller]")]
+    [ApiController]
+    public class inventoryController : Controller
     {
     }
 }
