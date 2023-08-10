4204ca44d705503302140d0e3a730304b3f05f25
HS <HS@HS>
2023-08-10 15:38:22 +0800

    Ver 1.1.26.0

--- 調劑台管理系統/Form1.cs
+++ 調劑台管理系統/Form1.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.1.25.0")]
-[assembly: AssemblyFileVersion("1.1.25.0")]
+[assembly: AssemblyVersion("1.1.26.0")]
+[assembly: AssemblyFileVersion("1.1.26.0")]
 namespace 調劑台管理系統
 {
 
