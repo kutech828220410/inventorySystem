6f96ad0192ca9db8088e2ea5903dbacb5de85946
useradmin <useradmin@TWINFO212>
2023-09-05 23:06:24 +0800

    1.1.31.26

--- 調劑台管理系統/Form1.cs
+++ 調劑台管理系統/Form1.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.1.31.25")]
-[assembly: AssemblyFileVersion("1.1.31.25")]
+[assembly: AssemblyVersion("1.1.31.26")]
+[assembly: AssemblyFileVersion("1.1.31.26")]
 namespace 調劑台管理系統
 {
 
