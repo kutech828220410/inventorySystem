82ea8927618cc1f3035bdb54df4ba8984a7fdb59
HS <HS@DESKTOP-OT506S0>
2024-03-13 16:03:35 +0800

    20240313

--- 調劑台管理系統/Form1.cs
+++ 調劑台管理系統/Form1.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.2.1.28")]
-[assembly: AssemblyFileVersion("1.2.1.28")]
+[assembly: AssemblyVersion("1.2.1.29")]
+[assembly: AssemblyFileVersion("1.2.1.29")]
 namespace 調劑台管理系統
 {
 
