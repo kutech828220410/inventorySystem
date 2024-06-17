d87f542d46752e7694b1079bc5ea2c22adf97bad
中國附醫-北港 <HS@HS>
2024-06-17 19:39:50 +0800

    "1.2.1.56"

--- 調劑台管理系統/Main_Form.cs
+++ 調劑台管理系統/Main_Form.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.2.1.55")]
-[assembly: AssemblyFileVersion("1.2.1.55")]
+[assembly: AssemblyVersion("1.2.1.56")]
+[assembly: AssemblyFileVersion("1.2.1.56")]
 namespace 調劑台管理系統
 {
 
