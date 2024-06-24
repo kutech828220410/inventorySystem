15f423fcee788f8f5ee2b9380029a98881018de1
hson_rdp <hson_rdp@HS>
2024-06-24 21:14:05 +0800

    20240624

--- HIS_WebApi/Properties/AssemblyInfo.cs
+++ HIS_WebApi/Properties/AssemblyInfo.cs
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.20240624211011")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.20240624114945")]
->>>>>>> 2ca9f79a8211488e6b290f48cfd1c0768f70de46
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.20240624211322")]
