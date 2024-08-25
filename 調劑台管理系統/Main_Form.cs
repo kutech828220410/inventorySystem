7e744dd22b29c1bb41436b83100b9d6dc0803ba1
HS_server <HS@HS>
2024-08-26 03:17:18 +0800

    Ver 1.2.2.7

--- 調劑台管理系統/Main_Form.cs
+++ 調劑台管理系統/Main_Form.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.2.2.6")]
-[assembly: AssemblyFileVersion("1.2.2.6")]
+[assembly: AssemblyVersion("1.2.2.7")]
+[assembly: AssemblyFileVersion("1.2.2.7")]
 namespace 調劑台管理系統
 {
 
