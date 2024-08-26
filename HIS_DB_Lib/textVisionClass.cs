c0674f9c55d7134b3c95017b92b254c00f545fcf
Anna2985 <hshiting58@gmail.com>
2024-08-26 09:44:46 +0800

    20240826

---
+++ HIS_DB_Lib/textVisionClass.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Basic;
+using System.Text.Json.Serialization;
+
+namespace HIS_DB_Lib
+{
+    [EnumDescription("textVision")]
+    public enum enum_textVision
+    {
+        [Description("GUID,VARCHAR,50,PRIMARY")]
+        GUID,
+        [Description("操作者姓名,VARCHAR,30,INDEX")]
+        操作者姓名,
+        [Description("操作者ID,VARCHAR,30,INDEX")]
+        操作者ID,
+        [Description("圖片,LONGTEXT,10,NONE")]
+        圖片,
+        [Description("操作時間,DATETIME,50,INDEX")]
+        操作時間,
+        [Description("Log,LONGTEXT,10,NONE")]
+        Log,
+        [Description("座標,VARCHAR,50,NONE")]
+        座標,
+        [Description("op_keyword,VARCHAR,100,NONE")]
+        op_keyword,
+        [Description("UI結果,VARCHAR,500,NONE")]
+        UI結果,
+        [Description("批號,VARCHAR,50,NONE")]
+        批號,
+        [Description("單號,VARCHAR,50,NONE")]
+        單號,
+        [Description("藥名,VARCHAR,50,NONE")]
+        藥名,
+        [Description("中文名,VARCHAR,50,NONE")]
+        中文名,
+        [Description("數量,VARCHAR,10,NONE")]
+        數量,
+        [Description("效期,DATETIME,50,NONE")]
+        效期,
+        [Description("藥品碼,VARCHAR,30,INDEX")]
+        藥品碼
+    }
+    /// <summary>
+    /// TextVision 資料
+    /// </summary>
+    public class textVisionClass
+    {
+        /// <summary>
+        /// 唯一KEY
+        /// </summary>
+        [JsonPropertyName("GUID")]
+        public string GUID { get; set; }
+        /// <summary>
+        /// 操作者姓名
+        /// </summary>
+        [JsonPropertyName("op_name")]
+        public string 操作者姓名 { get; set; }
+        /// <summary>
+        /// 操作者ID
+        /// </summary>
+        [JsonPropertyName("op_id")]
+        public string 操作者ID { get; set; }
+        /// <summary>
+        /// base64
+        /// </summary>
+        [JsonPropertyName("base64")]
+        public string 圖片 { get; set; }
+        /// <summary>
+        /// 操作時間
+        /// </summary>
+        [JsonPropertyName("op_time")]
+        public string 操作時間 { get; set; }
+        /// <summary>
+        /// Log
+        /// </summary>
+        [JsonPropertyName("logs")]
+        public object Log { get; set; }
+        /// <summary>
+        /// 座標
+        /// </summary>
+        [JsonPropertyName("roi")]
+        public object 座標 { get; set; }
+        /// <summary>
+        /// keyword
+        /// </summary>
+        [JsonPropertyName("op_keywords")]
+        public object op_keyword { get; set; }
+        /// <summary>
+        /// UI結果
+        /// </summary>
+        [JsonPropertyName("UI_result")]
+        public object UI結果 { get; set; }
+        /// <summary>
+        /// 批號
+        /// </summary>
+        [JsonPropertyName("batch_num")]
+        public string 批號 { get; set; }
+        /// <summary>
+        /// 單號
+        /// </summary>
+        [JsonPropertyName("po_num")]
+        public string 單號 { get; set; }
+        /// <summary>
+        /// 藥名
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string 藥名 { get; set; }
+        /// <summary>
+        /// 中文名
+        /// </summary>
+        [JsonPropertyName("cht_name")]
+        public string 中文名 { get; set; }
+        /// <summary>
+        /// 數量
+        /// </summary>
+        [JsonPropertyName("qty")]
+        public string 數量 { get; set; }
+        /// <summary>
+        /// 效期
+        /// </summary>
+        [JsonPropertyName("expirydate")]
+        public string 效期 { get; set; }
+        /// <summary>
+        /// 藥品碼
+        /// </summary>
+        [JsonPropertyName("CODE")]
+        public string 藥品碼 { get; set; }
+        static public List<textVisionClass> ai_analyze(List<textVisionClass> textVisionClasses)
+        {
+            string url = $"";
+
+            returnData returnData = new returnData();
+            returnData.Data = textVisionClasses;
+
+            string json_in = returnData.JsonSerializationt();
+            string json_out = Net.WEBApiPostJson(url, json_in);
+            returnData = json_out.JsonDeserializet<returnData>();
+            if (returnData == null) return null;
+            if (returnData.Code != 200) return null;
+            List<textVisionClass> out_textVisionClass = returnData.Data.ObjToClass<List<textVisionClass>>();
+            Console.WriteLine($"{returnData}");
+            return out_textVisionClass;
+        }
+    }
+}
---
+++ HIS_WebApi/(API)TextVision/PCMPO.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SQLUI;
+using Basic;
+using HIS_DB_Lib;
+using MySql.Data.MySqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HIS_WebApi._API_TextVision
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PCMPO : ControllerBase
+    {
+        static private string API_Server = "http://127.0.0.1:4433/api/serversetting";
+        static private MySqlSslMode SSLMode = MySqlSslMode.None;
+        [HttpPost("init_pcmpo")]
+        public string init_pcmpo([FromBody] returnData returnData)
+        {
+            MyTimerBasic myTimerBasic = new MyTimerBasic();
+            try
+            {
+                List<ServerSettingClass> serverSettingClasses = ServerSettingController.GetAllServerSetting();
+                serverSettingClasses = serverSettingClasses.MyFind("Main", "網頁", "人員資料");
+                if (serverSettingClasses.Count == 0)
+                {
+                    returnData.Code = -200;
+                    returnData.Result = $"找無Server資料!";
+                    return returnData.JsonSerializationt();
+                }
+                return CheckCreatTable(serverSettingClasses[0], new enum_textVision());
+            }
+            catch (Exception ex)
+            {
+                returnData.Code = -200;
+                returnData.Result = $"Exception : {ex.Message}";
+                return returnData.JsonSerializationt(true);
+            }
+
+        }
+        private string CheckCreatTable(ServerSettingClass serverSettingClass, Enum enumInstance)
+        {
+            string Server = serverSettingClass.Server;
+            string DB = serverSettingClass.DBName;
+            string UserName = serverSettingClass.User;
+            string Password = serverSettingClass.Password;
+            uint Port = (uint)serverSettingClass.Port.StringToInt32();
+
+            Table table = MethodClass.CheckCreatTable(serverSettingClass, enumInstance);
+            return table.JsonSerializationt(true);
+        }
+        [HttpPost("analyze")]
+        public string analyze([FromBody] returnData returnData)
+        {
+            MyTimerBasic myTimerBasic = new MyTimerBasic();
+            returnData.Method = "analyze";
+            try 
+            {
+                List<ServerSettingClass> serverSettingClasses = ServerSettingController.GetAllServerSetting();
+                serverSettingClasses = serverSettingClasses.MyFind("Main", "網頁", "人員資料");
+                if (serverSettingClasses.Count == 0)
+                {
+                    returnData.Code = -200;
+                    returnData.Result = $"找無Server資料";
+                    return returnData.JsonSerializationt();
+                }
+
+                string Server = serverSettingClasses[0].Server;
+                string DB = serverSettingClasses[0].DBName;
+                string UserName = serverSettingClasses[0].User;
+                string Password = serverSettingClasses[0].Password;
+                uint Port = (uint)serverSettingClasses[0].Port.StringToInt32();
+
+                SQLControl sQLControl_textVision = new SQLControl(Server, DB, "textVision", UserName, Password, Port, SSLMode);
+                List<textVisionClass> input_textVision = returnData.Data.ObjToClass<List<textVisionClass>>();
+                input_textVision[0].GUID = Guid.NewGuid().ToString();
+                input_textVision[0].操作時間 = DateTime.Now.ToDateTimeString();
+
+                List<object[]> sql_textVision = input_textVision.ClassToSQL<textVisionClass, enum_textVision>();
+                sQLControl_textVision.AddRows(null, sql_textVision);
+
+                List<textVisionClass> aiResult = textVisionClass.ai_analyze(input_textVision);
+
+
+
+                returnData.Code = 200;
+                returnData.Data = "";
+                returnData.TimeTaken = $"{myTimerBasic}";
+                returnData.Method = "";
+                returnData.Result = $"";
+                return returnData.JsonSerializationt(true);
+            }
+            catch(Exception ex)
+            {
+                returnData.Code = -200;
+                returnData.Result = $"Exception : {ex.Message}";
+                return returnData.JsonSerializationt(true);
+            }
+            
+
+        }
+
+
+
+    }
+}
