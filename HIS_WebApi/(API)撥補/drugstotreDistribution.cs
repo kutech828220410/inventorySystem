17871901f5707806c769aa57b45dced02de5b5a0
HS_server <HS@HS>
2024-10-07 18:10:58 +0800

    20241007

--- HIS_WebApi/(API)撥補/drugstotreDistribution.cs
+++ HIS_WebApi/(API)撥補/drugstotreDistribution.cs
@@ -50,6 +50,7 @@ namespace HIS_WebApi
         /// <param name="returnData">共用傳遞資料結構</param>
         /// <returns></returns>
         [Route("init")]
+        [Swashbuckle.AspNetCore.Annotations.SwaggerResponse(1, "", typeof(drugStotreDistributionClass))]
         [HttpPost]
         public string POST_init([FromBody] returnData returnData)
         {
