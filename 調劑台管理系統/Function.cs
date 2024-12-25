ce004647001d1479d1ff5dbe36c6f2f33f487a7b
HS <HS@DESKTOP-QVKUL3S>
2024-12-25 16:39:36 +0800

    20241225

--- 調劑台管理系統/Function.cs
+++ 調劑台管理系統/Function.cs
@@ -1513,6 +1513,11 @@ namespace 調劑台管理系統
                             list_IP.Add(IP);
                         }
                     }
+                    else if (device.DeviceType == DeviceType.RFID_Device)
+                    {
+                        list_IP.Add(IP);
+                        list_lock_IP.Add(IP);
+                    }
                 }
             }
 
