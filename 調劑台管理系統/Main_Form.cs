04e3c9f01d8b644366949a065e7eac509f253279
HS <HS@HS>
2025-03-02 15:35:59 +0800

    20250302

--- 調劑台管理系統/Main_Form.cs
+++ 調劑台管理系統/Main_Form.cs
@@ -20,8 +20,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.0.25.0204")]
-[assembly: AssemblyFileVersion("1.0.25.0204")]
+[assembly: AssemblyVersion("1.0.25.0302")]
+[assembly: AssemblyFileVersion("1.0.25.0302")]
 namespace 調劑台管理系統
 {
 
