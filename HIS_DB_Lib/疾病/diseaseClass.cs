966c6d35ab43304300ddaad61a8ebdf36f028c0f
Anna2985 <hshiting58@gamil.com>
2025-05-13 15:38:45 +0800

    20250513

---
+++ HIS_DB_Lib/疾病/diseaseClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Basic;
+using System.ComponentModel;
+using System.Web;
+using NPOI.SS.Formula.Functions;
+
+namespace HIS_DB_Lib
+{
+    public enum enum_disease_EXCEL
+    {
+        code,
+        英文名稱,
+        中文名稱,
+    }
+    [EnumDescription("disease")]
+    public enum enum_disease
+    {
+        [Description("GUID,VARCHAR,50,PRIMARY")]
+        GUID,
+        [Description("疾病代碼,VARCHAR,50,INDEX")]
+        疾病代碼,
+        [Description("中文說明,VARCHAR,200,NONE")]
+        中文說明,
+        [Description("英文說明,VARCHAR,400,NONE")]
+        英文說明,
+    }
+    
+
+    public class diseaseClass
+    {
+        [JsonPropertyName("GUID")]
+        public string GUID { get; set; }
+        [JsonPropertyName("ICD")]
+        public string 疾病代碼 { get; set; }
+        [JsonPropertyName("CHT")]
+        public string 中文說明 { get; set; }
+        [JsonPropertyName("EN")]
+        public string 英文說明 { get; set; }
+        static public List<diseaseClass> get_by_ICD(string API_Server, string ICD)
+        {
+            string url = $"{API_Server}/api/disease/get_by_ICD";
+
+            returnData returnData = new returnData();
+            returnData.ValueAry.Add(ICD);
+            string json_in = returnData.JsonSerializationt();
+            string json_out = Net.WEBApiPostJson(url, json_in);
+            returnData = json_out.JsonDeserializet<returnData>();
+            if (returnData == null) return null;
+            if (returnData.Code != 200) return null;
+            List<diseaseClass> out_diseaseClassClass = returnData.Data.ObjToClass<List<diseaseClass>>();
+            Console.WriteLine($"{returnData}");
+            return out_diseaseClassClass;
+        }
+        static public Dictionary<string, List<diseaseClass>> ToDictByICD(List<diseaseClass> diseaseClasses)
+        {
+            Dictionary<string, List<diseaseClass>> dictionary = new Dictionary<string, List<diseaseClass>>();
+            foreach (var item in diseaseClasses)
+            {
+                if (dictionary.TryGetValue(item.疾病代碼, out List<diseaseClass> list))
+                {
+                    list.Add(item);
+                }
+                else
+                {
+                    dictionary[item.疾病代碼] = new List<diseaseClass> { item };
+                }
+            }
+            return dictionary;
+        }
+        static public List<diseaseClass> GetByICD(Dictionary<string, List<diseaseClass>> dict, string master_GUID)
+        {
+            if (dict.TryGetValue(master_GUID, out List<diseaseClass> diseaseClasses))
+            {
+                return diseaseClasses;
+            }
+            else
+            {
+                return new List<diseaseClass>();
+            }
+        }
+    }
+}
---
+++ HIS_WebApi/(API)疾病/disease.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HIS_DB_Lib;
+using Basic;
+using MySql.Data.MySqlClient;
+using SQLUI;
+using System.Collections.Concurrent;
+using System.Text.Json.Serialization;
+using H_Pannel_lib;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using NPOI.HPSF;
+using NPOI.HSSF.Util;
+using System.Text.RegularExpressions;
+using System.Data;
+using MyUI;
+using System.IO;
+using HIS_DB_Lib;
+using System.Net;
+using Google.Protobuf;
+using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp.Drawing;
+using MyOffice;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+using NPOI.SS.Formula.Functions;
+using Google.Protobuf.WellKnownTypes;
+using SkiaSharp;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HIS_WebApi._API_疾病
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class disease : ControllerBase
+    {
+        static string API_Server = Method.GetServerAPI("Main", "網頁", "API01");
+        static private MySqlSslMode SSLMode = MySqlSslMode.None;
+        /// <summary>
+        /// 初始化資料庫
+        /// </summary>
+        /// <remarks>
+        /// {
+        ///     
+        /// }
+        /// </remarks>
+        /// <param name="returnData">共用傳遞資料結構</param>
+        /// <returns></returns>
+        [Swashbuckle.AspNetCore.Annotations.SwaggerResponse(1, "", typeof(diseaseClass))]
+        [HttpPost("init")]
+        public string init()
+        {
+            try
+            {
+                return CheckCreatTable(null, new enum_disease());
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [HttpPost("load")]
+        public string load([FromBody] returnData returnData)
+        {
+            try
+            {
+
+                string filePath = (@"./ICD10-CM.csv");
+                //byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+                List<object[]> list_value = new List<object[]>();
+                List<diseaseClass> diseaseClasses = new List<diseaseClass>();
+
+                using (TextFieldParser parser = new TextFieldParser(filePath, Encoding.UTF8))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    parser.HasFieldsEnclosedInQuotes = true;
+
+                    bool isFirstLine = true;
+                    while (!parser.EndOfData)
+                    {
+                        string[] fields = parser.ReadFields();
+                        if (isFirstLine)
+                        {
+                            isFirstLine = false; // 跳過標題列
+                            continue;
+                        }
+                        list_value.Add(fields);
+                    }
+                }
+                (string Server, string DB, string UserName, string Password, uint Port) = HIS_WebApi.Method.GetServerInfo("Main", "網頁", "VM端");
+                SQLControl sQLControl = new SQLControl(Server, DB, "disease", UserName, Password, Port, SSLMode);
+                List<object[]> sqlData = sQLControl.GetAllRows(null);
+                List<diseaseClass> diseases = sqlData.SQLToClass<diseaseClass, enum_disease>();
+                Dictionary<string, List<diseaseClass>> dict = diseaseClass.ToDictByICD(diseases);
+                for (int i = 0; i < list_value.Count; i++)
+                {
+                    diseaseClass diseaseClass = diseaseClass.GetByICD(dict, list_value[i][(int)enum_disease_EXCEL.code].ObjectToString()).FirstOrDefault();
+                    if (diseaseClass != null) continue;
+                    
+                    diseaseClass add_diseaseClass = new diseaseClass
+                    {
+                        GUID = Guid.NewGuid().ToString(),
+                        疾病代碼 = list_value[i][(int)enum_disease_EXCEL.code].ObjectToString(),
+                        中文說明 = list_value[i][(int)enum_disease_EXCEL.中文名稱].ObjectToString(),
+                        英文說明 = list_value[i][(int)enum_disease_EXCEL.英文名稱].ObjectToString()
+                    };
+                    diseaseClasses.Add(add_diseaseClass);
+                }
+                init();
+                
+                List<object[]> add = diseaseClasses.ClassToSQL<diseaseClass, enum_disease>();
+                //sQLControl.AddRows(null,add);
+                
+                returnData.Data = diseaseClasses;
+                return returnData.JsonSerializationt(true);
+                
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [HttpPost("get_by_ICD")]
+        public string get_by_ICD([FromBody] returnData returnData)
+        {
+            MyTimerBasic myTimerBasic = new MyTimerBasic();
+            returnData.Method = "get_by_ICD";
+            try
+            {
+                returnData.RequestUrl = Method.GetRequestPath(HttpContext, includeQuery: false);
+
+                if (returnData.ValueAry.Count != 1)
+                {
+                    returnData.Code = -200;
+                    returnData.Result = $"returnData.ValueAry應該為[\"疾病代碼\"]";
+                    return returnData.JsonSerializationt();
+                }
+                string 疾病代碼 = returnData.ValueAry[0];
+                string[] 疾病代碼_Array = 疾病代碼.Split(";");
+                (string Server, string DB, string UserName, string Password, uint Port) = HIS_WebApi.Method.GetServerInfo("Main", "網頁", "VM端");
+
+                string TableName = "disease";
+                SQLControl sQLControl = new SQLControl(Server, DB, TableName, UserName, Password, Port, SSLMode);                            
+                //string command = $"SELECT * FROM {DB}.{TableName} WHERE 疾病代碼  = '{疾病代碼}';"; 
+                string command = $"SELECT * FROM {DB}.{TableName} WHERE"; 
+
+                for (int i = 0; i < 疾病代碼_Array.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        command += $" 疾病代碼  = '{疾病代碼_Array[i]}'";
+                    }
+                    else if(i == 疾病代碼_Array.Length-1)
+                    {
+                        command += $" OR 疾病代碼 = '{疾病代碼_Array[i]}';";
+                    }
+                    else
+                    {
+                        command += $" OR 疾病代碼 = '{疾病代碼_Array[i]}'";
+                    }
+                }
+                DataTable dataTable = sQLControl.WtrteCommandAndExecuteReader(command);           
+                List<object[]> list_diseaseClass = dataTable.DataTableToRowList();
+                List<diseaseClass> suspiciousRxLogClasses = list_diseaseClass.SQLToClass<diseaseClass, enum_disease>();
+                if(suspiciousRxLogClasses == null)
+                {
+                    returnData.Data = new List<diseaseClass>();
+                }
+                else
+                {
+                    returnData.Data = suspiciousRxLogClasses;
+                }
+                returnData.Code = 200;
+                returnData.TimeTaken = $"{myTimerBasic}";
+                returnData.Result = $"取得疾病資料共<{suspiciousRxLogClasses.Count}>筆";
+                return returnData.JsonSerializationt(true);
+            }
+            catch (Exception e)
+            {
+                returnData.Code = -200;
+                returnData.Result = e.Message;
+                return returnData.JsonSerializationt();
+            }
+        }
+
+        private string CheckCreatTable(string tableName, System.Enum Enum)
+        {
+            if (tableName == null)
+            {
+                tableName = Enum.GetEnumDescription();
+            }
+            SQLUI.Table table = new SQLUI.Table(tableName);
+            List<sys_serverSettingClass> sys_serverSettingClasses = ServerSettingController.GetAllServerSetting();
+            sys_serverSettingClasses = sys_serverSettingClasses.MyFind("Main", "網頁", "藥檔資料");
+            if (sys_serverSettingClasses.Count == 0)
+            {
+                return $"找無Server資料!";
+            }
+            table = MethodClass.CheckCreatTable(sys_serverSettingClasses[0], Enum);
+
+            return table.JsonSerializationt(true);
+        }
+        
+        
+    }
+}
