fbb6e7b6ca639797f89d320b74268762f8441f22
高榮門診藥局 <HS@HS>
2025-06-24 14:44:46 +0800

    20250624V3

--- 調劑台管理系統/Main_Form.cs
+++ 調劑台管理系統/Main_Form.cs
@@ -20,8 +20,8 @@ using System.Runtime.InteropServices;
 using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
-[assembly: AssemblyVersion("1.0.25.062241")]
-[assembly: AssemblyFileVersion("1.0.25.06241")]
+[assembly: AssemblyVersion("1.0.25.062243")]
+[assembly: AssemblyFileVersion("1.0.25.06243")]
 namespace 調劑台管理系統
 {
 
