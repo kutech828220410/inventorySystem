07892ff7c8e24157542fe7ffd9a5c4cc1fd42c75
Anna2985 <hshiting58@gamil.com>
2025-09-08 18:57:35 +0800

    20250908

--- HIS_WebApi/(API)系統/controlpanel.cs
+++ HIS_WebApi/(API)系統/controlpanel.cs
@@ -16,12 +16,12 @@ namespace HIS_WebApi._API_系統
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class bbs : ControllerBase
+    public class controlpanel : ControllerBase
     {
         static private MySqlSslMode SSLMode = MySqlSslMode.None;
-        private string tableName = "bbs";
+        private string tableName = "controlpanel";
 
-        [Swashbuckle.AspNetCore.Annotations.SwaggerResponse(200, "bbsClass物件", typeof(bbs))]
+        [Swashbuckle.AspNetCore.Annotations.SwaggerResponse(200, "controlpanelClass物件", typeof(controlpanel))]
         /// <summary>
         ///初始化資料庫
         /// </summary>
@@ -60,7 +60,7 @@ namespace HIS_WebApi._API_系統
         ///     "Data": 
         ///     
         ///         {
-        ///             bbsClass物件
+        ///             controlpanelClass物件
         ///         }
         ///     
         ///     "Value": "",
@@ -87,14 +87,14 @@ namespace HIS_WebApi._API_系統
                     returnData.Result = $"returnData.Data資料錯誤!";
                     return returnData.JsonSerializationt();
                 }
-                bbsClass bbsClass = returnData.Data.ObjToClass<bbsClass>();
-                if (bbsClass == null)
+                controlpanelClass controlpanelClass = returnData.Data.ObjToClass<controlpanelClass>();
+                if (controlpanelClass == null)
                 {
                     returnData.Code = -200;
-                    returnData.Result = "returnData.Data資料錯誤，須為 bbsClass物件!";
+                    returnData.Result = "returnData.Data資料錯誤，須為 controlpanelClass物件!";
                     return returnData.JsonSerializationt();
                 }
-                if(bbsClass.公告開始時間.StringIsEmpty() || bbsClass.公告結束時間.StringIsEmpty())
+                if(controlpanelClass.公告開始時間.StringIsEmpty() || controlpanelClass.公告結束時間.StringIsEmpty())
                 {
                     returnData.Code = -200;
                     returnData.Result = "公告開始時間、公告結束時間不得為空";
@@ -102,14 +102,14 @@ namespace HIS_WebApi._API_系統
                 }
                 (string Server, string DB, string UserName, string Password, uint Port) = await HIS_WebApi.Method.GetServerInfoAsync("Main", "網頁", "VM端");
                 SQLControl sQLControl = new SQLControl(Server, DB, tableName, UserName, Password, Port, SSLMode);
-                bbsClass.GUID = Guid.NewGuid().ToString();
-                bbsClass.建立時間 = DateTime.Now.ToDateTimeString();
-                object[] add = bbsClass.ClassToSQL<bbsClass, enum_bbs>();               
+                controlpanelClass.GUID = Guid.NewGuid().ToString();
+                controlpanelClass.建立時間 = DateTime.Now.ToDateTimeString();
+                object[] add = controlpanelClass.ClassToSQL<controlpanelClass, enum_controlpanel>();               
                 await sQLControl.AddRowAsync(null, add);
 
 
                 returnData.Code = 200;
-                returnData.Data = bbsClass;
+                returnData.Data = controlpanelClass;
                 returnData.TimeTaken = myTimerBasic.ToString();
                 returnData.Method = "add";
                 returnData.Result = $"資料寫入成功!";
@@ -131,7 +131,7 @@ namespace HIS_WebApi._API_系統
         ///     "Data": 
         ///     
         ///         {
-        ///             bbsClass物件
+        ///             controlpanelClass物件
         ///         }
         ///     
         ///     "Value": "",
@@ -157,11 +157,11 @@ namespace HIS_WebApi._API_系統
                     returnData.Result = $"returnData.Data資料錯誤!";
                     return returnData.JsonSerializationt();
                 }
-                bbsClass bbsClass_input = returnData.Data.ObjToClass<bbsClass>();
+                controlpanelClass bbsClass_input = returnData.Data.ObjToClass<controlpanelClass>();
                 if (bbsClass_input == null)
                 {
                     returnData.Code = -200;
-                    returnData.Result = "returnData.Data資料錯誤，須為 bbsClass物件!";
+                    returnData.Result = "returnData.Data資料錯誤，須為 controlpanelClass物件!";
                     return returnData.JsonSerializationt();
                 }
                 if (bbsClass_input.公告開始時間.StringIsEmpty() || bbsClass_input.公告結束時間.StringIsEmpty())
@@ -172,14 +172,14 @@ namespace HIS_WebApi._API_系統
                 }
                 (string Server, string DB, string UserName, string Password, uint Port) = await HIS_WebApi.Method.GetServerInfoAsync("Main", "網頁", "VM端");
                 SQLControl sQLControl = new SQLControl(Server, DB, tableName, UserName, Password, Port, SSLMode);
-                List<object[]> objects = await sQLControl.GetRowsByDefultAsync(null,(int)enum_bbs.GUID, bbsClass_input.GUID);
+                List<object[]> objects = await sQLControl.GetRowsByDefultAsync(null,(int)enum_controlpanel.GUID, bbsClass_input.GUID);
                 if(objects.Count() == 0)
                 {
                     returnData.Code = -200;
                     returnData.Result = $"查無此GUID({bbsClass_input.GUID}) 資料";
                     return returnData.JsonSerializationt();
                 }
-                bbsClass bbs = objects[0].SQLToClass<bbsClass, enum_bbs>();
+                controlpanelClass bbs = objects[0].SQLToClass<controlpanelClass, enum_controlpanel>();
                 if (bbsClass_input.主旨.StringIsEmpty() == false && bbsClass_input.主旨 != bbs.主旨) bbs.主旨 = bbsClass_input.主旨;
                 if (bbsClass_input.內容.StringIsEmpty() == false && bbsClass_input.內容 != bbs.內容) bbs.內容 = bbsClass_input.內容;
                 if (bbsClass_input.重要程度.StringIsEmpty() == false && bbsClass_input.重要程度 != bbs.重要程度) bbs.重要程度 = bbsClass_input.重要程度;
@@ -191,7 +191,7 @@ namespace HIS_WebApi._API_系統
                 bbs.建立時間 = DateTime.Now.ToDateTimeString();
 
 
-                object[] update = bbs.ClassToSQL<bbsClass, enum_bbs>();
+                object[] update = bbs.ClassToSQL<controlpanelClass, enum_controlpanel>();
                 await sQLControl.UpdateRowAsync(null, update);
 
 
@@ -218,7 +218,7 @@ namespace HIS_WebApi._API_系統
         ///     "Data": 
         ///     
         ///         {
-        ///             bbsClass物件
+        ///             controlpanelClass物件
         ///         }
         ///     
         ///     "Value": "",
@@ -244,25 +244,25 @@ namespace HIS_WebApi._API_系統
                     returnData.Result = $"returnData.Data資料錯誤!";
                     return returnData.JsonSerializationt();
                 }
-                bbsClass bbsClass_input = returnData.Data.ObjToClass<bbsClass>();
+                controlpanelClass bbsClass_input = returnData.Data.ObjToClass<controlpanelClass>();
                 if (bbsClass_input == null)
                 {
                     returnData.Code = -200;
-                    returnData.Result = "returnData.Data資料錯誤，須為 bbsClass物件!";
+                    returnData.Result = "returnData.Data資料錯誤，須為 controlpanelClass物件!";
                     return returnData.JsonSerializationt();
                 }
                 (string Server, string DB, string UserName, string Password, uint Port) = await HIS_WebApi.Method.GetServerInfoAsync("Main", "網頁", "VM端");
                 SQLControl sQLControl = new SQLControl(Server, DB, tableName, UserName, Password, Port, SSLMode);
-                List<object[]> objects = await sQLControl.GetRowsByDefultAsync(null, (int)enum_bbs.GUID, bbsClass_input.GUID);
+                List<object[]> objects = await sQLControl.GetRowsByDefultAsync(null, (int)enum_controlpanel.GUID, bbsClass_input.GUID);
                 if (objects.Count() == 0)
                 {
                     returnData.Code = -200;
                     returnData.Result = $"查無此GUID({bbsClass_input.GUID}) 資料";
                     return returnData.JsonSerializationt();
                 }
-                bbsClass bbs = objects[0].SQLToClass<bbsClass, enum_bbs>();
+                controlpanelClass bbs = objects[0].SQLToClass<controlpanelClass, enum_controlpanel>();
       
-                object[] delete = bbs.ClassToSQL<bbsClass, enum_bbs>();
+                object[] delete = bbs.ClassToSQL<controlpanelClass, enum_controlpanel>();
                 await sQLControl.DeleteRowAsync(null, delete);
 
 
@@ -281,7 +281,7 @@ namespace HIS_WebApi._API_系統
             }
         }
         /// <summary>
-        /// 取得公告時間區間內的資料
+        /// 取得公告時間區間內的資料(公告欄)
         /// </summary>
         /// <remarks>
         /// <code>
@@ -302,8 +302,8 @@ namespace HIS_WebApi._API_系統
         /// </remarks>
         /// <param name="returnData">共用傳遞資料結構</param>
         /// <returns></returns>
-        [HttpPost("get_by_startendtime")]
-        public async Task<string> get_by_startendtime([FromBody] returnData returnData, CancellationToken ct = default)
+        [HttpPost("get_by_startendtime_bbs")]
+        public async Task<string> get_by_startendtime_bbs([FromBody] returnData returnData, CancellationToken ct = default)
         {
             MyTimerBasic myTimerBasic = new MyTimerBasic();
             try
@@ -315,16 +315,72 @@ namespace HIS_WebApi._API_系統
                 SQLControl sQLControl = new SQLControl(Server, DB, tableName, UserName, Password, Port, SSLMode);
                 string command = @$"SELECT * FROM {DB}.{tableName}
                                     WHERE 公告開始時間 <= '{now}' 
-                                    AND 公告結束時間 >= '{now}' ;";
+                                    AND 公告結束時間 >= '{now}'
+                                    AND 主旨 NOT IN('缺貨通知');";
                 List<object[]> objects = await sQLControl.WriteCommandAsync(command,ct);
-                List<bbsClass> bbsClasses = objects.SQLToClass<bbsClass, enum_bbs>();
-                bbsClasses.Sort(new bbsClass.ICP_By_ct_time());
+                List<controlpanelClass> bbsClasses = objects.SQLToClass<controlpanelClass, enum_controlpanel>();
+                bbsClasses.Sort(new controlpanelClass.ICP_By_ct_time());
 
                 returnData.Code = 200;
                 returnData.Data = bbsClasses;
                 returnData.TimeTaken = myTimerBasic.ToString();
                 returnData.Method = "get_by_startendtime";
-                returnData.Result = $"取得公告時間內資料，共{bbsClasses.Count}筆!";
+                returnData.Result = $"取得公佈欄公告時間內資料，共{bbsClasses.Count}筆!";
+                return await returnData.JsonSerializationtAsync(true);
+            }
+            catch (Exception ex)
+            {
+                returnData.Code = -200;
+                returnData.Result = ex.Message;
+                return await returnData.JsonSerializationtAsync(true);
+            }
+        }
+        /// <summary>
+        /// 取得公告時間區間內的資料(缺貨通知)
+        /// </summary>
+        /// <remarks>
+        /// <code>
+        ///   {
+        ///     "Data": 
+        ///     [
+        ///         {
+        ///         }
+        ///     ]
+        ///     "Value": "",
+        ///     "ValueAry":[]
+        ///     "TableName": "",
+        ///     "ServerName": "",
+        ///     "ServerType": "",
+        ///     "TimeTaken": ""
+        ///   }
+        /// </code>
+        /// </remarks>
+        /// <param name="returnData">共用傳遞資料結構</param>
+        /// <returns></returns>
+        [HttpPost("get_by_startendtime_out_of_stock")]
+        public async Task<string> get_by_startendtime_out_of_stock([FromBody] returnData returnData, CancellationToken ct = default)
+        {
+            MyTimerBasic myTimerBasic = new MyTimerBasic();
+            try
+            {
+                await init(returnData);
+                string now = DateTime.Now.ToDateTimeString();
+
+                (string Server, string DB, string UserName, string Password, uint Port) = await HIS_WebApi.Method.GetServerInfoAsync("Main", "網頁", "VM端");
+                SQLControl sQLControl = new SQLControl(Server, DB, tableName, UserName, Password, Port, SSLMode);
+                string command = @$"SELECT * FROM {DB}.{tableName}
+                                    WHERE 公告開始時間 <= '{now}' 
+                                    AND 公告結束時間 >= '{now}'
+                                    AND 主旨 IN('缺貨通知');";
+                List<object[]> objects = await sQLControl.WriteCommandAsync(command, ct);
+                List<controlpanelClass> bbsClasses = objects.SQLToClass<controlpanelClass, enum_controlpanel>();
+                bbsClasses.Sort(new controlpanelClass.ICP_By_ct_time());
+
+                returnData.Code = 200;
+                returnData.Data = bbsClasses;
+                returnData.TimeTaken = myTimerBasic.ToString();
+                returnData.Method = "get_by_startendtime";
+                returnData.Result = $"取得缺貨通知公告時間內資料，共{bbsClasses.Count}筆!";
                 return await returnData.JsonSerializationtAsync(true);
             }
             catch (Exception ex)
@@ -366,8 +422,8 @@ namespace HIS_WebApi._API_系統
                 SQLControl sQLControl = new SQLControl(Server, DB, tableName, UserName, Password, Port, SSLMode);
 
                 List<object[]> objects = await sQLControl.GetAllRowsAsync(null);
-                List<bbsClass> bbsClasses = objects.SQLToClass<bbsClass, enum_bbs>();
-                bbsClasses.Sort(new bbsClass.ICP_By_ct_time());
+                List<controlpanelClass> bbsClasses = objects.SQLToClass<controlpanelClass, enum_controlpanel>();
+                bbsClasses.Sort(new controlpanelClass.ICP_By_ct_time());
 
                 returnData.Code = 200;
                 returnData.Data = bbsClasses;
@@ -411,7 +467,7 @@ namespace HIS_WebApi._API_系統
             MyTimerBasic myTimerBasic = new MyTimerBasic();
             try
             {
-                List<string> level = System.Enum.GetNames(typeof(enum_bbs_ReportLevel)).ToList();
+                List<string> level = System.Enum.GetNames(typeof(enum_controlpanelLevel)).ToList();
                 returnData.Code = 200;
                 returnData.Data = level;
                 returnData.TimeTaken = myTimerBasic.ToString();
@@ -441,7 +497,7 @@ namespace HIS_WebApi._API_系統
 
             List<Table> tables = new List<Table>();
 
-            tables.Add(MethodClass.CheckCreatTable(sys_serverSettingClasses[0], new enum_bbs()));
+            tables.Add(MethodClass.CheckCreatTable(sys_serverSettingClasses[0], new enum_controlpanel()));
 
             return tables.JsonSerializationt(true);
         }
